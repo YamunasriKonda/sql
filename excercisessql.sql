c99677080ffc5617d3113cdac55f15b8d9bd70a7
yamunasrikonda <yamuna.jakkam91@gmail.com>
2022-09-22 16:43:30 +0100

    pj

--- excercisessql.sql
+++ excercisessql.sql
@@ -4,4 +4,43 @@
  select * from city;
  select * from country;
  select * from countrylanguage;
+ #(q1)
+select count(*) from city where countrycode =  "usa";
+#(q2)
+select population , lifeExpectancy from country where code="ARG";
+#(q3)
+select city.Name from country join city on country.capital = city.id
+where country.Name  =" Spain ";
+
+#(q 10)
+select * from country order by SurfaceArea desc limit 10;
+#(q 14)
+select distinct Language from countrylanguage ;
+#(q 19)
+select GovernmentForm, count(*) from country group by GovernmentForm order by count(*) desc limit 3;
+
+
+
+
+
+
+
+
+
+
+select * from city;
+select * from country;
+select * from countrylanguage;
+
+
+
+
+
+
+
+
+
+ 
+ 
+ 
  
