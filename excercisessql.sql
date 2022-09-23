71c93260ebb00d94ce6b1d7aea47c6e257c831df
yamunasrikonda <yamuna.jakkam91@gmail.com>
2022-09-23 11:29:28 +0100

    kk

--- excercisessql.sql
+++ excercisessql.sql
@@ -14,10 +14,26 @@ where country.Name  =" Spain ";
 
 #(q 10)
 select * from country order by SurfaceArea desc limit 10;
+#(q11)
+select Name from city order by Population desc limit 5;
+
+select * from city ;
+
+
+
+
+
 #(q 14)
 select distinct Language from countrylanguage ;
 #(q 19)
 select GovernmentForm, count(*) from country group by GovernmentForm order by count(*) desc limit 3;
+#(q20)
+select count (Name) from country  order by  IndepYear >0;
+#(q5)
+select c1. language from countryc left join countrylanguage c1 on c.code = c1.countrycode  where c.region="southeast Asia";
+#(Q6)
+select name from city where name like "F%" limit 25;
+#(q8)
 
 
 
@@ -26,9 +42,6 @@ select GovernmentForm, count(*) from country group by GovernmentForm order by co
 
 
 
-
-
-select * from city;
 select * from country;
 select * from countrylanguage;
 
